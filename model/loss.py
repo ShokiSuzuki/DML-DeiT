07d15cc436d341de63e515ed75b1eccc14330d56
ShokiSuzuki <shoki655@outlook.jp>
2021-11-23 13:52:41 +0900

    Update loss function

--- model/loss.py
+++ model/loss.py
@@ -18,61 +18,40 @@ class DMLLoss(torch.nn.Module):
         self.output_dir = output_dir
 
     def forward(self, model_id, outputs, labels, output_loss: bool=False):
-
         T = self.tau
-        for i in range(len(outputs)):
-            if i == model_id:
-                continue
-            if not isinstance(outputs[i], torch.Tensor):
-                output, output_kd = outputs[i]
-                outputs[i] = (output + output_kd) / 2
 
         outputs1    = outputs[model_id]
         outputs1_kd = None
-        if not isinstance(outputs[model_id], torch.Tensor):
-            outputs1, outputs1_kd = outputs[model_id]
+        if not isinstance(outputs1, torch.Tensor):
+            outputs1, outputs1_kd = outputs1
 
         base_loss = self.base_criterion(outputs1, labels)
         if self.distillation_type == 'none' or len(outputs) == 1:
             return base_loss
 
         distillation_loss = 0.0
-        if self.distillation_type == 'soft':
-            if outputs1_kd is not None:
-                for i in range(len(outputs)):
-                    if i == model_id:
-                        continue
-                    distillation_loss += F.kl_div(
-                        F.log_softmax(outputs1_kd / T, dim=1),
-                        F.log_softmax(outputs[i].detach() / T, dim=1),
-                        reduction='batchmean',
-                        log_target=True
-                    ) * (T * T)
-            else:
-                for i in range(len(outputs)):
-                    if i == model_id:
-                        continue
-                    distillation_loss += F.kl_div(
-                        F.log_softmax(outputs1 / T, dim=1),
-                        F.log_softmax(outputs[i].detach() / T, dim=1),
-                        reduction='batchmean',
-                        log_target=True
-                    ) * (T * T)
-        else:
-            if outputs1_kd is not None:
-                for i in range(len(outputs)):
-                    if i == model_id:
-                        continue
+        if outputs1_kd is not None:
+            for i in range(len(outputs)):
+                if i == model_id:
+                    continue
+
+                if self.distillation_type == 'soft':
+                    distillation_loss += kl_div(outputs1_kd, outputs[i], T)
+                else:
                     distillation_loss += F.cross_entropy(outputs1_kd, outputs[i].argmax(dim=1))
-            else:
-                for i in range(len(outputs)):
-                    if i == model_id:
-                        continue
+        else:
+            for i in range(len(outputs)):
+                if i == model_id:
+                    continue
+
+                if self.distillation_type == 'soft':
+                    distillation_loss += kl_div(outputs1, outputs[i], T)
+                else:
                     distillation_loss += F.cross_entropy(outputs1, outputs[i].argmax(dim=1))
-        
+
         distillation_loss = distillation_loss / (len(outputs) - 1)
         loss = base_loss + distillation_loss
-            
+
         if output_loss:
             log_stats = {'base_loss': base_loss.item(), 'distillation_loss': distillation_loss.item()}
 
@@ -82,3 +61,15 @@ class DMLLoss(torch.nn.Module):
 
         return loss
 
+
+def kl_div(output1, output2, T):
+    if not isinstance(output2, torch.Tensor): # tuple to tensor
+        output2 = (output2[0] + output2[1]) / 2
+
+    loss = F.kl_div(
+                F.log_softmax(output1 / T, dim=1),
+                F.log_softmax(output2.detach() / T, dim=1),
+                reduction='batchmean',
+                log_target=True
+            ) * (T * T)
+    return loss
